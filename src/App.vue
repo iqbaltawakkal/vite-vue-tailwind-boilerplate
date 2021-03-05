4f63ca0b8661fecdc3cbd2d1d9e98243c35fe052
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-03-05 17:36:18 +0700

    add tailwind + example + remove template style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img class="m-auto mt-20" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld  msg="Hello Vue 3 + Vite" />
 </template>
 
 <script setup>
@@ -9,14 +9,3 @@ import HelloWorld from './components/HelloWorld.vue'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-center">{{ msg }}</h1>
 
-  <p>
+  <p class="text-green-500 text-center">
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a> |
+    <a href="https://tailwindcss.com/" target="_blank">Tailwind Documentation</a>
   </p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
+  <button class="bg-green-500 hover:bg-green-700 text-white p-2 rounded mx-auto block" @click="state.count++">count is: {{ state.count }}</button>
+  <p class="text-center">
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
@@ -22,9 +23,3 @@ defineProps({
 
 const state = reactive({ count: 0 })
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
