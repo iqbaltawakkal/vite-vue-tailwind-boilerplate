28ce1447a758ee356fb55745092a9565ecce29ab
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-09-10 11:56:31 +0700

    updated eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-recommended", "prettier", "@vue/prettier"],
+  extends: ["plugin:vue/vue3-essential", "prettier", "@vue/prettier"],
   plugins: ["vue"],
   rules: { "prettier/prettier": ["error", { endOfLine: "auto" }] },
 };
